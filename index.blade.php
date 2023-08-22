9eb94d6d14f64e48e1a64261cf4a421f06f91051
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2023-08-22 15:22:24 +0200

    Update index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -37,7 +37,7 @@
                                   </div>
                                   @if (Auth::user()->email_verified_at != null)
                                     <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-700/10">
-                                        {{ __('navigation.verified') }}
+                                        {{ __('Verified') }}
                                     </span>
                                   @endif
                               </div>
